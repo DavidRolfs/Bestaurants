ed51b47994b29b7d5a9e97a55ac67501425a0ed0
Jordan  Loop and david rolfs <jordan+david@>
2017-06-08 11:28:01 -0700

    add restaurant form and view all restaurants page.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,18 +8,43 @@ namespace Bestaurants
   {
     public HomeModule()
     {
+//  root -> index
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
 
+//  cusines/add -> add-cuisine
       Get["/cuisines/add"] = _ => {
         return View["add-cuisine.cshtml"];
       };
+
+//  cusines/add -> all-cuisine
       Post["/cuisines/all"] = _ => {
         Cuisine newCuisine = new Cuisine(Request.Form["cuisine-name"]);
         newCuisine.Save();
         List<Cuisine> allCuisines = Cuisine.GetAll();
         return View["all-cuisine.cshtml", allCuisines];
+      };
+
+//    restaurants/add -> add-restaurants
+      Get["/restaurants/add"] = _ => {
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["add-restaurants.cshtml", allCuisines];
+      };
+
+//    restaurants/add -> all-restaurants
+      Post["/restaurants/all"] = _ => {
+        Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["cuisine-id"]);
+        newRestaurant.Save();
+        return View["index.cshtml"];
+      };
+      Get["/cuisines/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Cuisine SelectedCuisine = Cuisine.Find(parameters.id);
+        List<Restaurant> CuisineRestaurants = SelectedCuisine.GetRestaurant();
+        model.Add("Cuisine", SelectedCuisine);
+        model.Add("restaurants", CuisineRestaurants);
+        return View["cuisine-restaurants.cshtml", model];
       };
     }
   }
